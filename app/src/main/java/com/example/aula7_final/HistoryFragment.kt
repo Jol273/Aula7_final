5f04a043d45630e97b7df895573b8e08d05a3f52
Filipe Silva <a21705317@alunos.ulht.pt>
2020-04-08 22:06:18 +0100

    Erro no historico resolvido

--- app/src/main/java/com/example/aula7_final/HistoryFragment.kt
+++ app/src/main/java/com/example/aula7_final/HistoryFragment.kt
@@ -7,29 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.fragment_history.*
+import kotlinx.android.synthetic.main.fragment_history.view.*
+
+val operations = mutableListOf(Operation("1+1",2.0),Operation("2+3",5.0))
 
 class HistoryFragment : Fragment() {
 
-    val operations = mutableListOf(Operation("1+1",2.0),Operation("2+3",5.0))
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_history, container, false)
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        view.list_history.adapter = HistoryAdapter(activity as Context, R.layout.item_expression, operations)
+        view.list_history.layoutManager = LinearLayoutManager(activity as Context)
 
-        val operations = intent.getParcelableArrayListExtra<Operation>(EXTRA_HISTORY)
-
-
-
-    }*/
-
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-            val view = inflater.inflate(R.layout.fragment_history, container, false)
-
-            list_history.adapter = HistoryAdapter(activity as Context, R.layout.item_expression, operations)
-            list_history.layoutManager = LinearLayoutManager(activity as Context)
-
-            return view
-        }
+        return view
+    }
 
 
 }
